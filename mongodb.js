333725fbca825c19ad0fa83a6ca1e9a3359614e0
SanduNicu <sandu.nicu94@gmail.com>
2020-01-20 23:35:59 +0200

    added mongoose & user and task models

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,29 @@
-const mongodb = require('mongodb');
-const { MongoClient } = mongodb;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connnct to database!');
   } 
 
   console.log('Connected to database!');
+  const db = client.db(databaseName);
+  db.collection('users').updateOne({
+    _id: new ObjectID('5e2192f749baa03b0ef43aab')
+  }, {
+    $set: {
+      name: 'Ali',
+    }
+  }).then((result) => {
+    console.log(result);
+  })
+    .catch((err) => {
+      console.log(err);
+    })
 });
